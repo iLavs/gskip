0b5118198f535c43fe5441616575b795319eadd6
Igor Lavskij <i.lavskij@gmail.com>
2017-06-21 01:31:14 +0300

    create basic layout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-$(document).ready(function() {
-  $('a[href="#"]').click(function (e){
-    e.preventDefault();
-  });
+function affix() {
+    var scrollValue = $(window).scrollTop();
 
-  
+    if (scrollValue > 70) {
+        $('#stickyHeader').addClass('affix');
+    }
+    else {
+        $('#stickyHeader').removeClass('affix');
+    }
+}
+
+$(document).ready(function () {
+    $('a[href="#"]').click(function (e) {
+        e.preventDefault();
+    });
+
+    affix();
+
+    $(window).on('scroll', function (event) {
+        setTimeout(function () {
+            affix();
+        }, 100);
+
+    });
 });
